da425e3525cf75024dc607184f9388cee1dada49
Nannaan <nonodera65@gmail.com>
2023-05-20 22:58:59 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
+  
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :condition_id, :delivery_id, :prefecture_id, :schedule_id, :price, :user).merge(user_id: current_user.id)
+  end
+
 end
