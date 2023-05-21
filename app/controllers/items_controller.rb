bcc95de2bdc2b646fa03de825298bacba19c0d04
Nannaan <nonodera65@gmail.com>
2023-05-21 22:56:56 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
   end
 
   private
@@ -8,5 +14,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explain, :category_id, :condition_id, :delivery_id, :prefecture_id, :schedule_id, :price, :user).merge(user_id: current_user.id)
   end
-
 end
