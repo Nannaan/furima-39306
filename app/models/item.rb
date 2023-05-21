14e489c8d28d328dc9d73f65c2b3606763446379
Nannaan <nonodera65@gmail.com>
2023-05-21 21:31:12 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :name,          presence: true
+  validates :explain,       presence: true
+  validates :price,         presence: true
+  validates :user,          presence: true
+
   # ActiveHashに関連するファイル
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -10,13 +15,11 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :schedule
 
-  validates :name,          presence: true
-  validates :explain,       presence: true
-  validates :category_id,   presence: true
-  validates :condition_id,  presence: true
-  validates :delivery_id,   presence: true
-  validates :prefecture_id, presence: true
-  validates :schedule_id,   presence: true
-  validates :price,         presence: true
-  validates :user,          presence: true
+  #「---」の時は保存できないようにする
+  validates :category_id,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :schedule_id,   numericality: { other_than: 1 , message: "can't be blank"}
+
 end
