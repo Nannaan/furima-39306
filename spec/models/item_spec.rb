2d70f4e1a871efa9f2ea91e86750dab42290d034
Nannaan <nonodera65@gmail.com>
2023-05-22 17:36:42 +0900

    テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品情報の保存' do
+    context '出品できるとき' do
+      it "全ての情報が存在すれば登録できる" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できないとき' do
+      it "商品画像が空では登録できない" do
+        # imageのテストコードの記述！
+      end
+      it "商品名が空では登録できない" do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品の説明が空では登録できない" do
+        @item.explain = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explain can't be blank")
+      end
+      it "カテゴリーが---では登録できない" do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "商品の状態が---では登録できない" do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it "配送料が---では登録できない" do
+        @item.delivery_id= '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank")
+      end
+      it "発送元地域が---では登録できない" do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "配送日数が---では登録できない" do
+        @item.schedule_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Schedule can't be blank")
+      end
+      it "価格が空では登録できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "ユーザーが紐付いていないと登録できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
