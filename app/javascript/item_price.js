b1608557a41954502fada8d6897a0e1e519f1f00
Nannaan <nonodera65@gmail.com>
2023-05-22 20:45:20 +0900

    価格表示の設定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    // 価格
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    // 販売手数料
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+    // 販売利益
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - inputValue * 0.1)
+  })
 });
