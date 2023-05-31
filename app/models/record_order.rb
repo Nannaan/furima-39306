fc1f8d5249536a7ec4aef42f3d323f87b7e2f542
Nannaan <nonodera65@gmail.com>
2023-05-31 12:40:36 +0900

    バリデーションの修正

--- app/models/record_order.rb
+++ app/models/record_order.rb
@@ -10,7 +10,7 @@ class RecordOrder
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
   end
   
 
