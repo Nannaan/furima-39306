ba20d542a83c0a77ada6f811af0f20e191f30a6c
Nannaan <nonodera65@gmail.com>
2023-06-02 17:31:03 +0900

    with_optionsに修正

--- app/models/record_order.rb
+++ app/models/record_order.rb
@@ -5,12 +5,12 @@ class RecordOrder
   with_options presence: true do
     validates :user_id
     validates :item_id
-
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :token
   end
 
   def save
@@ -22,5 +22,4 @@ class RecordOrder
                  phone_number: phone_number, record_id: record.id)
   end
 
-  validates :token, presence: true
 end
